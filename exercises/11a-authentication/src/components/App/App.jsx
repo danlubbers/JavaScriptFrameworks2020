c98f4ac86b046979b8222e3a816034d636e80397
Dan Lubbers <lubbers.dan@gmail.com>
2020-04-01 20:33:18 -0400

    Finished 11a-authentication

--- exercises/11a-authentication/src/components/App/App.jsx
+++ exercises/11a-authentication/src/components/App/App.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import LoggedInContent from "../LoggedInContent/LoggedInContent";
 // You may need to import additional things here
+import axios from 'axios';
 
 function App() {
   /**
@@ -16,80 +17,97 @@ function App() {
   /**
    * Be sure to set this when a user tries to login with invalid credentials.
    */
-  const [errorMessage, setErrorMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState();
 
   /**
    * You may need to add more things to state
    */
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(!!localStorage.getItem('token'));
 
   const login = token => {
-    /**
-     * Complete me
-     */
+    localStorage.setItem('token', token);
+    setIsUserLoggedIn(true);
+    setIsLoading(false);
   };
-
+  
   const logout = () => {
-    /**
-     * Complete me
-     */
+    localStorage.removeItem('token')
+    setIsUserLoggedIn(false);
   };
 
   const handleLoginRequest = e => {
-    /**
-     * Complete me.
-     */
+    e.preventDefault();
+    setIsLoading(true);
+
+    axios("http://localhost:7000/jwt/login", {
+      method: "POST", 
+      data: {
+        username, 
+        password
+      }
+    })
+      .then(res => login(res.data.token))
+      .catch(err => setErrorMessage(err.response.data.message));
   };
 
   /**
    * If the user is logged in, you should render the <LoggedInContent /> component instead.
    */
+  
   return (
-    <div className="container mt-2 mb-5">
-      <h1>Login</h1>
-      <form className="form-inline mb-2" onSubmit={handleLoginRequest}>
-        <div className="form-group">
-          <label htmlFor="username" className="mr-2">
-            Username
-          </label>
-          <input
-            type="text"
-            id="username"
-            className="form-control mr-3"
-            required={true}
-            value={username}
-            onChange={e => setUsername(e.target.value)}
-          />
+    <>
+    {isUserLoggedIn 
+      ? 
+        <LoggedInContent logout={logout}/> 
+      : 
+        <div className="container mt-2 mb-5">
+          <h1>Login</h1>
+          <form className="form-inline mb-2" onSubmit={handleLoginRequest}>
+            <div className="form-group">
+              <label htmlFor="username" className="mr-2">
+                Username
+              </label>
+              <input
+                type="text"
+                id="username"
+                className="form-control mr-3"
+                required={true}
+                value={username}
+                onChange={e => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password" className="mr-2">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                className="form-control mr-3"
+                required={true}
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary" disabled={isLoading}>
+              Login
+            </button>
+          </form>
+          <p className="form-text">
+            <small>
+              The username is <em>username</em> and the password is{" "}
+              <em>password</em>
+            </small>
+          </p>
+          {isLoading && <p>Loading ...</p>}
+          {errorMessage && (
+            <div className="alert alert-danger" role="alert">
+              {errorMessage}
+            </div>
+          )}
         </div>
-        <div className="form-group">
-          <label htmlFor="password" className="mr-2">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            className="form-control mr-3"
-            required={true}
-            value={password}
-            onChange={e => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary" disabled={isLoading}>
-          Login
-        </button>
-      </form>
-      <p className="form-text">
-        <small>
-          The username is <em>username</em> and the password is{" "}
-          <em>password</em>
-        </small>
-      </p>
-      {isLoading && <p>Loading ...</p>}
-      {errorMessage && (
-        <div className="alert alert-danger" role="alert">
-          {errorMessage}
-        </div>
-      )}
-    </div>
+      } 
+    </>
   );
 }
 
